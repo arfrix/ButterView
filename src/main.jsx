18191b097f2e877f9e9cfefdf7e963e647522183
arfa <arfa.madi@tapsi.cab>
2022-10-19 00:04:49 +0330

    basic routing is added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Feed from "./features/feed/Feed";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Feed />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
